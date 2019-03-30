ffab8f60595dcf21bf82d343310763b56e3a240e
Михаил Киселев <mvkiselev@group.legion.ru>
2019-03-30 17:45:51 +0300

    version 0.1

---
+++ javascript/lesson1.js
@@ -0,0 +1,24 @@
+﻿var sum = (function () {
+
+	"use strict"
+
+	var run_sum;
+	run_sum = 0;
+
+	function fx_run_sum(a) {
+        
+		if (arguments.length === 0) {
+			console.log(run_sum);
+			run_sum = 0;
+		}
+		else {
+			if (typeof a === 'number') {
+				run_sum = run_sum + a;
+				return fx_run_sum;
+            		}
+		}
+	}
+
+	return fx_run_sum;
+
+})();
