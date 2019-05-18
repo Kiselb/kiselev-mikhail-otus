22d58a337256000fa8c9e64c1bd50bf288b2a14a
Михаил Киселев <mvkiselev@group.legion.ru>
2019-05-18 18:28:35 +0300

    Version 0.58

---
+++ node/lesson9/getPath.js
@@ -0,0 +1,33 @@
+﻿		function getPath(element) {
+
+
+
+			var css_selector = "";
+
+			if (typeof element === undefined) {
+
+				return "";
+
+			}
+
+			if (element.parentElement === document.documentElement) {
+
+
+				return "body";
+
+
+			}
+
+			var parent_element = element.parentElement;
+	
+			for(var i = 0; i < parent_element.children.length; i++) {
+				if (element === parent_element.children[i]) {
+			
+		css_selector = getPath(parent_element) + " > :nth-child(" + (i + 1).toString() + ")";
+				}
+			}
+
+			return css_selector;
+
+		};
+
