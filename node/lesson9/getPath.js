493074b8bbdee94994ec1ab16df8db2c3ccde7a6
Михаил Киселев <mvkiselev@group.legion.ru>
2019-05-20 17:16:35 +0300

    Version 0.77

--- node/lesson9/getPath.js
+++ node/lesson9/getPath.js
@@ -1,33 +1,50 @@
 		function getPath(element) {
 
-
-
+			var origin_element = element;
 			var css_selector = "";
 
 			if (typeof element === undefined) {
 
-				return "";
+				return undefined;
 
 			}
-
-			if (element.parentElement === document.documentElement) {
-
+			if (element instanceof HTMLBodyElement) {
 
 				return "body";
 
-
 			}
+			if (element instanceof HTMLElement) {
 
-			var parent_element = element.parentElement;
-	
-			for(var i = 0; i < parent_element.children.length; i++) {
-				if (element === parent_element.children[i]) {
-			
-		css_selector = getPath(parent_element) + " > :nth-child(" + (i + 1).toString() + ")";
+				if (typeof element.id.length > 0) {
+
+					return "#" + element.id;
+
 				}
+
+				var is_element = true;
+
+				while(is_element) {
+
+					var parent_element = origin_element.parentElement;
+
+					for(var i = 0; i < parent_element.children.length; i++) {
+						if (origin_element === parent_element.children[i]) {
+							css_selector = " > :nth-child(" + (i + 1).toString() + ")" + css_selector;
+							break;
+						}
+					}
+
+					origin_element = parent_element;
+					is_element = !(parent_element instanceof HTMLBodyElement);
+				}
+
+				css_selector = "body " + css_selector
+
+				return css_selector;
+
 			}
 
-			return css_selector;
+			return undefined;
 
 		};
 
