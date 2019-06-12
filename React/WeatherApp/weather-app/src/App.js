c914fb057d70334d76acdf02548c789bd593b764
Михаил Киселев <mvkiselev@group.legion.ru>
2019-06-12 19:03:43 +0300

    Version 0.94

--- React/WeatherApp/weather-app/src/App.js
+++ React/WeatherApp/weather-app/src/App.js
@@ -86,7 +86,7 @@ const weather_on_cities = [
     weather: [
       { description: 'солнечно', temperature: 32, wind_dir: 'ЮЗ', wind_vel: 2, pressure: 765, humidity: 50},
       { description: 'солнечно', temperature: 31, wind_dir: 'ЮЗ', wind_vel: 2, pressure: 765, humidity: 50},
-      { description: 'солнечно', temperature: 25, wind_dir: 'ЮЗ', wind_vel: 2, pressure: 765, humidity: 50},
+      { description: 'солнечно', temperature: 28, wind_dir: 'ЮЗ', wind_vel: 2, pressure: 765, humidity: 50},
       { description: 'солнечно', temperature: 25, wind_dir: 'ЮЗ', wind_vel: 2, pressure: 765, humidity: 50}
     ]
   },
