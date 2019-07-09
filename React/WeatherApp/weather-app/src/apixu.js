cb3eb7ab0bb35af3f6027c6bab03e87dc5cc8038
Михаил Киселев <mvkiselev@group.legion.ru>
2019-07-09 19:05:40 +0300

    Version 1.2

--- React/WeatherApp/weather-app/src/apixu.js
+++ React/WeatherApp/weather-app/src/apixu.js
@@ -1,15 +1,10 @@
 
 const APIXU_URL = (cityname) => (`http://api.apixu.com/v1/forecast.json?key=01b1d58699d7441eb7133613191006&q=${cityname}&days=5`);
 
-export const getCityData = (err, res, cityname) => {
-    fetch(APIXU_URL(cityname))
+export const getCityData = (err, res, cityname) => fetch(APIXU_URL(cityname))
         .then(response => {
             if (response.ok) return response.json();
             throw Error("Communication error");
         })
-        .then(json => {
-            res(json);
-        })
+        .then(json => res(json))
         .catch(e => err(e));
-}
-
