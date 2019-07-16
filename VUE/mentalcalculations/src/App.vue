26f42a53dfd84ccb0e39771cdbcc70f248d060b0
Михаил Киселев <mvkiselev@group.legion.ru>
2019-07-16 20:34:30 +0300

    Version 1.78

--- VUE/mentalcalculations/src/App.vue
+++ VUE/mentalcalculations/src/App.vue
@@ -12,21 +12,21 @@
 </script>
 
 <style>
-body {
-  background-color: silver;
-}
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  /*margin-top: 60px;*/
-  position: relative;
-  width: 600px;
-  height: 750px;
-  margin: auto auto;
-  overflow: hidden;
-  background-color: white;   
-}
+  body {
+    background-color: silver;
+  }
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    /*margin-top: 60px;*/
+    position: relative;
+    width: 600px;
+    height: 750px;
+    margin: auto auto;
+    overflow: hidden;
+    background-color: white;   
+  }
 </style>
